bbf23f3e3076adb0a833249e5ca20d86d90b5510
XYZ <info@andrasnagy.xyz>
2018-02-01 21:58:48 +0100

    comma

--- js/data.js
+++ js/data.js
@@ -45,7 +45,7 @@ var DATA = [
         subtitle: "post FX, audio reactive",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/at5COmu4y80' frameborder='0' allowfullscreen></iframe></div>",
         text: "",
-        image: "supernem_01.gif"
+        image: "supernem_01.gif",
     },
     {
         category: "mapping",
