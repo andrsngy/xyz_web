8d4029b0ebf717365049a4b8456f1b7f06e706be
XYZ <info@andrasnagy.xyz>
2019-02-10 22:42:42 +0100

    .

--- js/data.js
+++ js/data.js
@@ -20,7 +20,7 @@ var DATA = [
         title: "HEIM ROLAND - EXTENSION",
         subtitle: "story telling, chalk paint",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/oLcf8sNiNdM' frameborder='0' allowfullscreen></iframe></div>",
-        text: "A storytelling installation projected on HEIM ROLAND chalk drawings.",
+        text: "A storytelling installation projected on HEIM ROLAND chalk drawings",
         image: "heim_01.gif"
     },
     {
