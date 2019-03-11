56bedcf41cf70e8aaad1a0b5b823ca69771871fe
XYZ <info@andrasnagy.xyz>
2019-03-11 23:37:57 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,7 +10,7 @@ var DATA = [
     {
         category: "performance",
         title: "ATMODANCE -  Story Of Eternity",
-        subtitle: "interactive mapping atmodance",
+        subtitle: "visual mapping atmodance",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed//DqTW6-jay3s' frameborder='0' allowfullscreen></iframe></div>",
         text: "Twisting around",
         image: "atmodance.jpg"
