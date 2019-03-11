a788967f5b52c265695a351c07cf82e12ea2253c
XYZ <info@andrasnagy.xyz>
2019-03-11 23:28:59 +0100

    atmo3

--- js/data.js
+++ js/data.js
@@ -3,9 +3,9 @@ var DATA = [
         category: "performance",
         title: "ATMODANCE -  Story Of Eternity",
         subtitle: "interactive mapping atmodance",
-        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/276206820' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
+        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed//DqTW6-jay3s' frameborder='0' allowfullscreen></iframe></div>",
         text: "Twisting around",
-        image: "atmodance.mp4"
+        image: "atmodance.jpg"
     },
     {
         category: "installation",
