67a887bf6317da2fc3f43236b619c4367a3dd135
XYZ <info@andrasnagy.xyz>
2019-03-11 23:13:13 +0100

    atmodance

--- js/data.js
+++ js/data.js
@@ -1,5 +1,13 @@
 var DATA = [
     {
+        category: "performance",
+        title: "ATMODANCE -  Story Of Eternity",
+        subtitle: "interactive mapping atmodance",
+        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/276206820' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
+        text: "Twisting around",
+        image: "atmodance.jpg"
+    },
+    {
         category: "installation",
         title: "WE SENSE",
         subtitle: "interactive shadow installation",
