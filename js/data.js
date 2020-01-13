83ce1f8749f5f99b9527ded94105a47b4b937b34
Andras Nagy <nagyandrasvagyok@gmail.com>
2020-01-13 14:11:21 +0100

    Update data.js
    szovegek atirasa

--- js/data.js
+++ js/data.js
@@ -1,146 +1,146 @@
 var DATA = [
     {
         category: "installation",
-        title: "WE SENSE",
+        title: "XYZ - WE SENSE // 2018",
         subtitle: "interactive shadow installation",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/276206820' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
-        text: "Sound: Adrian Newgent <br>Dancers: Linda Telek, Anna Szilvási ",
+        text: "Music by: Adrian Newgent <br>Dancers: Linda Telek, Anna Szilvási <br>Concept & Installation: XYZ",
         image: "we_sense_01.jpg"
     },
     {
         category: "performance",
-        title: "ATMODANCE -  Story Of Eternity",
+        title: "ATMODANCE - STORY OF ETERNITY // 2018",
         subtitle: "visual mapping atmodance",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed//DqTW6-jay3s' frameborder='0' allowfullscreen></iframe></div>",
-        text: "",
+        text: "Presented by the MAD Company, the first wind theatre on Earth. <br>More on their website: http://mad.company",
         image: "atmodance.jpg"
     },
     {
         category: "mapping",
-        title: "LIMELIGHT - EVERGREEN @ SINGAPORE",
+        title: "LIMELIGHT - EVERGREEN @ SINGAPORE // 2018",
         subtitle: "projection mapping, 3D",
-        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/a5dNqtppMW8' frameborder='0' allowfullscreen></iframe></div>",
-        text: "1 minute animation about digital chaos turn into harmony for Limelight projection mapping.",
+        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/ShwcWu0no8Y' frameborder='0' allowfullscreen></iframe></div>",
+        text: "Concept: Istvan David, Csaba Vilagosi, Viktor Vicsek <br>Art Director: Csaba Vilagosi<br>Producer: Istvan David<br>Animation:<br>Csaba Világosi<br>Károly Körmöczi<br>Viktor Vicsek<br>András Nagy aka XYZ<br>Music: AMB<br>Sound Design: Fine Cut Bodies",
         image: "limelight_sing_01.jpg"
     },
     {
         category: "installation",
-        title: "HEIM ROLAND - EXTENSION",
+        title: "HEIM ROLAND - EXTENSION // 2018",
         subtitle: "story telling, chalk paint",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/oLcf8sNiNdM' frameborder='0' allowfullscreen></iframe></div>",
-        text: "A storytelling installation projected on HEIM ROLAND chalk drawings",
+        text: "Narrated by: Zoltán Rátóti<br>Concept & Drawings: Roland Heim<br>Sound: Adrian Newgent<br>Visual: Andras Nagy<br><br>Quotes:<br>Weöres Sándor: Tér és pont<br>Hamvas Béla: A kertészkedés lélektana<br>Weöres Sándor: A mozdulatlan utazás",
         image: "heim_01.gif"
     },
     {
         category: "mapping",
-        title: "LIMELIGHT - WHAT IF @ MELBOURNE",
+        title: "LIMELIGHT - WHAT IF @ MELBOURNE // 2018",
         subtitle: "projection mapping, 3D",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/1pg_yN3qkOU' frameborder='0' allowfullscreen></iframe></div>",
-        text: "1 minute animation about connection for Limelight projection mapping.",
+        text: "Creative concept: István Dávid, Csaba Világosi, Viktor Vicsek<br>Art Director: Csaba Világosi<br>Technical supervisor: Viktor Vicsek<br>3D modeling: Dániel Szalkó<br>Producer: István Dávid<br>Music: AMB<pr>Sound design: Fine Cut Bodies",
         image: "limelight_melb_01.jpg"
     },
     {
         category: "installation",
-        title: "KONSTELLATIO @ HELSINKI",
+        title: "KONSTELLATIO @ HELSINKI // 2018",
         subtitle: "audio visual, light art, light sculpture",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/251047337' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
-        text: "Robot lamp design synchronized to Bordos László Zsolt projection artwork.",
+        text: "Dedicated to the memory of Mika Vainio<br>Concept & aimation: László Zsolt Bordos<br>Sound: Mika Vainio<br>Light design: Andras Nagy aka XYZ<br>Lighting operator: Teo Lanerva",
         image: "helsinki_01.gif"
     },
     {
         category: "mapping",
-        title: "REALITY QUANTIZED @ MTA BUDAPEST",
+        title: "REALITY QUANTIZED @ MTA BUDAPEST // 2016",
         subtitle: "projection mapping, 3D",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/145723561' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
-        text: "My first individual experiment with 3D projection mapping.",
+        text: "Sound: Patrik Kiss<br>Concept @ animation: Andras Nagy",
         image: "mta_01.gif"
     },
     {
         category: "liveset",
-        title: "PRESENSE LIVE ACT - STELLAR @ SZIMPLA KERT",
+        title: "PRESENSE LIVE ACT - STELLAR @ SZIMPLA KERT // 2018",
         subtitle: "generative visual, live performance, mapping",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/OADnn88NNxo' frameborder='0' allowfullscreen></iframe></div>",
-        text: "Szimpla Lemming Program: Kopcsik Márton - Violin, Gabor Szabo - Sampling/programming, Marilia Pilti - Santouri, David Krolikowski - Drums",
+        text: "Violin: Kopcsik Márton<br>Sound design: Gabor Szabo<br>Santouri: Marilia Pilti<br>Drums:David Krolikowski<br> Visuals: Andras Nagy",
         image: "presense_szimpla_01.gif"
     },
     {
         category: "mapping",
-        title: "ARTVISION @ MOSCOW",
+        title: "ARTVISION @ MOSCOW // 2016",
         subtitle: "projection mapping",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/NE7u9alHZ8c' frameborder='0' allowfullscreen></iframe></div>",
-        text: "My first story telling mapping for a contest.",
+        text: "Sound: Patrik Kiss<br>Concept & Animation: Dániel Besnyő & Andras Nagy",
         image: "moscow_01.gif"
     },
     {
         category: "mapping",
-        title: "MVM @ BAZILIKA",
+        title: "MVM @ BAZILIKA // 2016",
         subtitle: "realtime, 4K, generative, audio reactive",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/201538337' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
-        text: "4K audio reactive mapping for a János Balázs concert.",
+        text: "Piano: János Balázs<br>Sound reactive animations: Andras Nagy",
         image: "mvm_01.gif"
     },
     {
         category: "installation",
-        title: "XYZ - D1SPL4C3 @ ROM",
+        title: "XYZ - D1SPL4C3 @ ROM // 2017",
         subtitle: "generative art, endless form",
         video: "",
-        text: "My first installation. Generated endless patterns projected to a canvas.",
+        text: "A generative installation<br>Concept & Programming: Andras Nagy",
         image: "displace_01.gif"
     },
     {
         category: "musicvideo",
-        title: "GOROMBA - ILLUSION",
+        title: "GOROMBA - ILLUSION // 2014",
         subtitle: "time symmetry, clock, illusion, monochrome",
-        text: "The very-very first animation I have ever created.",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/c79qkbJSveM' frameborder='0' allowfullscreen></iframe></div>",
+        text: "Music: Barnabás Murányi aka GOROMBA<br>Visual: Andras Nagy",
         image: "goromba_01.gif"
     },
     {
         category: "mapping",
-        title: "XYZ - RGB @ PÉCS",
+        title: "XYZ - RGB @ PÉCS // 2017",
         subtitle: "projection mapping, contest",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/O3LIsRw2x8I' frameborder='0' allowfullscreen></iframe></div>",
-        text: "The borning of digital light.",
+        text: "Sound: Bence Nagy aka Belle Belle<br>Concept & 3D animation: Andras Nagy",
         image: "zsolnay_01.gif"
     },
     {
         category: "musicvideo",
-        title: "SUPERNEM - VAN",
+        title: "SUPERNEM - VAN // 2015",
         subtitle: "post FX, audio reactive",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/at5COmu4y80' frameborder='0' allowfullscreen></iframe></div>",
-        text: "",
+        text: "Directed by: Dániel Besnyő<br>Camera: Gábor Tarcsay<br>Lighting: Gergő Geltz<br>VFX: András Nagy<br>Makeup: Blanka Mohácsi<br>Production manager: Laura Szabó",
         image: "supernem_01.gif"
     },
     {
         category: "liveset",
-        title: "PANTHA DU PRINCE - LIVE @ AKVÁRIUM",
+        title: "PANTHA DU PRINCE - LIVE @ AKVÁRIUM // 2016",
         subtitle: "generative visual, live performance, mapping",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/125677492' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
-        text: "My first live visual performance with audio reactive 3D renders.",
+        text: "Music: Pantha du Prince - A Nomad's Retreat<br>Visuals: Dániel Besnyő & Andras Nagy",
         image: "pantha_01.gif"
     },
     {
         category: "liveset",
-        title: "COLORSTAR - 20 YEAR ANNIVERSARY",
+        title: "COLORSTAR - COUNT YOUR LUCKY STARS // LIVE 2016 // A38",
         subtitle: "realtime sync, mapping, illusion",
-        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/undefined' frameborder='0' allowfullscreen></iframe></div>",
-        text: "Live connection between Ableton live and Touchdesigner in realtime.",
+        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/4uQoLtIgd5Y' frameborder='0' allowfullscreen></iframe></div>",
+        text: "Colorstar Band: András Kelet, Szabolcs Máté, Dániel Sándor, Ferenc Somogyi, Péter Szalay<br>Visuals: Dániel Besnyő & András Nagy",
         image: "color_01.gif"
     },
     {
         category: "lightart",
-        title: "START ROW INDEX @ MARSEILLE",
+        title: "START ROW INDEX @ MARSEILLE // 2017",
         subtitle: "projection mapping, light art, programming",
         video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/256456325' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>",
-        text: "I built a 2 dimension video content converter to robot lamp 3D movements.",
+        text: "Création Image/Lumière/Son : Cyril Meroni<br>Conception logiciel Lumière : András Nagy<br>Coordination : Pierre Emmanuel Reviron<br>Régie technique : Fréderic Bouchez",
         image: "vendome_01.jpg"
     },
     {
         category: "installation",
-        title: "AURORA BOREALIS @ BUDAPEST",
+        title: "AURORA BOREALIS @ BUDAPEST // 2017",
         subtitle: "light art, programming, audio reaction",
-        video: "",
-        text: "Audio reactive robot lamp programming for Suomi 100 event.",
+        video: "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/yhGsfC8fhKo' frameborder='0' allowfullscreen></iframe></div>",
+        text: "Media design: BORDOS László Zsolt & NAGY András<br>Sound design: Kasperi LAINE<br>Curator: Andrea Kovács<br>Producer: FinnAgora",
         image: "aurora_01.jpg"
     },
 ]
